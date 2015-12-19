32056087e4811a7bf068295e5841efa1acd3b40b
denkovalenko <deniskovalenko96@mail.ru>
2015-12-19 15:43:11 +0200

    Del Gender, fixed expression and other

--- MemberRMS/User.cs
+++ MemberRMS/User.cs
@@ -28,7 +28,6 @@ namespace MemberRMS
         public Nullable<System.DateTime> Birthday { get; set; }
         public string Telephone { get; set; }
         public string Mail { get; set; }
-       
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Cart> Cart { get; set; }
