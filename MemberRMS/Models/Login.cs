3fee4298bfd54471c94b233fd9799481eebeda3f
denkovalenko <deniskovalenko96@mail.ru>
2015-12-21 02:21:35 +0200

    Add comments, Remember me, Homme page and other

--- MemberRMS/Models/Login.cs
+++ MemberRMS/Models/Login.cs
@@ -8,9 +8,10 @@ namespace MemberRMS.Models
 {
     public class Login
     {
-        
+        //создание поляе в модели, для использования в контроллере
         public string Username { get; set; }
         public string Psssword { get; set; }
+        public bool RememberMe { get; set; }
 
     }
 }
